012a92a368f62e95ec560ffa1cf0c2138a149a2a
baricai <40843695+baricai@users.noreply.github.com>
2018-09-11 23:20:37 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var Enemy = function(x, y, movement) {
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
-  Enemy.prototype.update = function(dt) {
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -31,7 +31,7 @@ var Enemy = function(x, y, movement) {
     }
   };
   // Draw the enemy on the screen, required method for game
-  Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
 }
