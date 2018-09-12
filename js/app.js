9e132918422f347840f0b0a6888586409a190bde
baricai <40843695+baricai@users.noreply.github.com>
2018-09-11 22:59:16 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, movement) {
+class Enemy {
+  constructor(x, y, movement) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
@@ -12,7 +13,7 @@ var Enemy = function(x, y, movement) {
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
-  Enemy.prototype.update = function(dt) {
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -21,88 +22,87 @@ var Enemy = function(x, y, movement) {
     // resets position of enemy to move from left to right when player reaches destination
     if (this.x > 500) {
       this.x = -150;
-      this.movement = 150 + Math.floor(Math.random() * 505);
+      this.movement = 150 + Math.floor(Math.random() * 500);
     }
 
     // below code will check for any collisions between player and enemy
-    if (player.x < this.x + 60 && player.x + 37 > this.x && player.y < this.y + 25 && 30 + player.y > this.y) {
-      player.x = 200; 
-      player.y = 400; 
+    if (player.x < this.x + 60 &&
+      player.x + 37 > this.x &&
+      player.y < this.y + 25 &&
+      30 + player.y > this.y) {
+      player.x = 200; // re-aligns position.1
+      player.y = 400; // re-aligns position.2
     }
   };
   // Draw the enemy on the screen, required method for game
-  Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
 }
 
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, movement) {
+class Player {
+  constructor(x, y, movement) {
     this.x = x;
     this.y = y;
     this.movement = movement;
     this.sprite = 'images/char-boy.png';
   }
-  
-Player.prototype.update = function() {
-    
-    if (this.y > 360) {
-      this.y = 360;
+  update() {
+    // Below code will stop the player from moving off canvas
+    if (this.y > 380) {
+      this.y = 380;
     }
 
-    if (this.x > 420) {
-      this.x = 420;
+    if (this.x > 400) {
+      this.x = 400;
     }
 
     if (this.x < 0) {
       this.x = 0;
     }
-    
-    if (this.y<0) {
-      this.x = 200;
-      this.y = 360;  
-    }
-};
+  };
 
-  Player.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
 
-  
-  Player.prototype.handleInput = function(keyPress) {
+  // Maneuver around the board using standard arrow keys OR WASD keys
+  handleInput(keyPress) {
     switch (keyPress) {
       case 'left':
-        this.x -= this.movement + 40;
+        this.x -= this.movement + 50;
         break;
       case 'up':
-        this.y -= this.movement + 20;
+        this.y -= this.movement + 30;
         break;
       case 'right':
-        this.x += this.movement + 40;
+        this.x += this.movement + 50;
         break;
       case 'down':
-        this.y += this.movement + 20;
+        this.y += this.movement + 30;
         break;
     }
   };
-
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [];
+let allEnemies = [];
 
-// Position the enemies
-var enemyPosition = [40, 130, 220];
-var player = new Player(200, 400, 50);
-var enemy;
+// Position enemies to be created
+let enemyPosition = [50, 135, 220];
+let player = new Player(200, 400, 50);
+let enemy;
 
 enemyPosition.forEach(function (posY) {
-  enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 505));
+  enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 499));
   allEnemies.push(enemy);
 });
 
