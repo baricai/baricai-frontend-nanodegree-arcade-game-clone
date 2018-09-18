a8bd379465acef9d0e7bc8559234017a984226fa
baricai <40843695+baricai@users.noreply.github.com>
2018-09-18 06:18:06 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var Enemy = function(x, y, speed) {
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.speed = Math.floor((Math.random()* 9) + 2);
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
